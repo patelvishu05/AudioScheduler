e1ab7556aabd021d4fe87fc0a8b296b510ee0fd3
patelvishu05 <35935851+patelvishu05@users.noreply.github.com>
2019-11-21 21:42:01 -0600

    structure change

--- AudioScheduler/src/application/Main.java
+++ AudioScheduler/src/application/Main.java
@@ -1,25 +1,32 @@
 package application;
-	
+
+import java.net.URL;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
+	
+	public static Stage stage;
+	
 	@Override
 	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+		stage = primaryStage;
+		URL url = getClass().getResource("../view/MainView.fxml");
+		FXMLLoader loader = new FXMLLoader(url);
+//		MainController controller = MainController.getInstance();
+		loader.setController(controller);
+		Parent rootNode = loader.load();
+		controller.setBorderPane((BorderPane) rootNode);
+		stage.setScene(new Scene(rootNode));
+		stage.show();
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
